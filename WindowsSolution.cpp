adeb0f0a48ee00beebcb445003ff0b5ca9e4ed03
joshikamlesh55 <72187420+joshikamlesh55@users.noreply.github.com>
2020-10-17 12:54:31 +0530

    Create WindowsSolution.cpp

---
+++ WindowsSolution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+// To execute C++, please define "int main()"
+int main()
+{
+    int test_cases;
+    cin>>test_cases;
+    while(test_cases--){
+      int n;
+      cin>>n;
+      // 3*k + 5*m + 7*x
+      // 10 3 + 3 + 3 + (1)
+      // 3 + 3+ 3+ 3+ 2
+      if(n%3==0){
+        cout<<n/3<<" 0 0\n";
+      } else if(n%3==2){
+        n = n - 5;
+        if(n<0) {
+          cout<<-1<<endl;
+      } else cout<<n/3<<" 1 0\n";
+    }--
+}
+  return 0;
+}
